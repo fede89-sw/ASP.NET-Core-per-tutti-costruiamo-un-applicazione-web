fa9865ed5127ce23edf56994d9dc439038b6874e
Federico Milani <fedemilani.89@gmail.com>
2021-04-09 17:13:53 +0200

    Aggiornata l'applicazione ad ASP.NET Core 2.2

--- MyCourse/Startup.cs
+++ MyCourse/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MyCourse
 {
@@ -16,6 +17,7 @@ namespace MyCourse
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc(); // devo aggiungere questo 'service' per usare il 'routing'
+            // services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

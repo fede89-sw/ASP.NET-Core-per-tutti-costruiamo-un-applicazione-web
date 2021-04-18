71d52eb59ba8d22e093269d4aa4a1814b143341b
Federico Milani <fedemilani.89@gmail.com>
2021-04-18 11:27:03 +0200

    Mapping OwnedType e campo Relazionale

--- MyCourse/Models/Services/Infrastructure/MyCourseDbContext.cs
+++ MyCourse/Models/Services/Infrastructure/MyCourseDbContext.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace MyCourse.Models.Entities
 {
@@ -50,6 +48,56 @@ namespace MyCourse.Models.Entities
                 // entity.HasKey(course => new {course.Id, course.Author}); // se la PK fosse composta da più campi la specifichi cosi
 
                 // finchè il nome delle proprietà della classe sono = al nome della colonna del DB posso anche non specificare il campo
+                
+                #region Mapping degli OwnedType
+                /*
+                Mapping di un OwnedType, ovvero di 'FullPrice' e 'CurrentPrice', che sono classi di tipo 'Money' contenenti le proprietà
+                'Amount' e 'Currency'; OwnedType sono cioè classi che ci aiutano a tenere coesi più valori e non sono Enitità, ovvero
+                non hanno ID quindi non sono tabelle di un Database, ma campi. 
+                Si mappano con 'OwnsOne' e se rispetti la convenzione basta fare solo 'entity.OwnsOne(course => course.CurrentPrice);'; infatti 
+                dato che EFCore trova un OwnedType che nel nostro caso è di tipo 'Money'con proprietà 'Amount' e 'Currency'va a cercare 
+                nel database colonne che si chiamano 'CurrentPrice_Amount' e 'CurrentPrice_Currency'.
+                Se avessi dato nomi diversi alle colonne nel database per questi valori, invece della riga sotto il mapping sarebbe stato:
+
+                entity.OwnsOne(course => course.CurrentPrice, builder => {
+                    builder.Property(money => money.Currency).HasColumnName("NomeColonna_Currency");
+                    builder.Property(money => money.Amount).HasColumnName("NomeColonna_Amount");
+                });
+                */
+
+                // entity.OwnsOne(course => course.CurrentPrice);
+
+                // avendo noi un campo di tipo 'enum', ovvero 'Currency', che nella tabella del database è convertito in tipo 'TEXT',
+                // dobbiamo informare EFCore della conversione da fare quando scrive su database e viceversa.
+                entity.OwnsOne(course => course.CurrentPrice, builder => {
+                    builder.Property(money => money.Currency).HasConversion<string>();
+                });
+
+                entity.OwnsOne(course => course.FullPrice, builder => {
+                    builder.Property(money => money.Currency).HasConversion<string>();
+                });
+                #endregion
+
+                #region Mapping per le Relazioni (1 a Molti, ...)
+                /*
+                Il campo 'Lessons' nell'entità(Tabella) 'Courses' è un campo relazionale, da cui posso accedere
+                a tutte le lezioni di un corso, dal corso stesso. Devo però mapparlo adeguatamente, come campo di Relazione.
+                */
+                entity.HasMany(course => course.Lessons)
+                        .WithOne(lesson => lesson.Course)
+                        .HasForeignKey(lesson => lesson.CourseId); // superfluo se il campo si chiama CourseId
+                // L'entità Courses ha molte Lessons, a cui posso accedere tramite campo 'Lessons' della sua tabella;
+                // a loro volta le lezione hanno solo un Course a cui fare riferimento, ed è il campo Course presente nella loro tabella(related_name di Django)
+                // Specifico in aggiunta che la ForeignKey in Lessons è il campo CourseId appositamente creato, ma seguendo le convezioni è 
+                // superfluo farlo in quanto se si chiama come l'entità a cui fa riferimento più il soffito Id (appunto CourseId) EFCore
+                // cerca già automanticamente quel campo
+                
+                // Il mapping delle relazioni lo fai solo una volta, o dal punto di vista di Courses come abbiamo fatto noi o nell'entità
+                // Lessons; in quel caso sarebbe stata:
+
+                // entity.HasOne(course => course.Course)
+                //         .WithMany(lesson => lesson.Lessons);
+                #endregion
 
                 //la region mi permette di creare un area che posso comprimere come visualizzazione in VSCode
                 #region mapping generato automaticamente dal tool di reverse engineering (DatabaseFirst) 
